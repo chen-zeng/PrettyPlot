9e92987e1359cfc14d1e299b049cb5321e9be3e7
Chen Zeng <chen.zeng@centre.edu>
2020-09-26 00:11:36 +0800

    Editted the plot to make it look pretty

--- js/pretty.js
+++ js/pretty.js
@@ -70,8 +70,17 @@ var makeTranslateString = function(x,y)
 var drawAxes = function(graphDim,margins,
                          xScale,yScale)
 {
-   
+   var xAxis = d3.axisBottom(xScale);
+    var yAxis = d3.axisLeft(yScale);
+ var axes=d3.select("svg")
+     .append("g")
  
+ axes.append("g")
+ .attr("transform","translate("+margins.left+","+(margins.top+graphDim.height)+")")
+    .call(xAxis)
+    axes.append("g")
+    .attr("transform","translate("+margins.left+","+(margins.top)+")")
+    .call(yAxis)
 }
 
 
@@ -79,14 +88,48 @@ var drawAxes = function(graphDim,margins,
 //margins - objedct that stores the size of the margins
 var drawLabels = function(graphDim,margins)
 {
+    var labels = d3.select("svg")
+    .append("g")
+    .classed("labels",true)
     
+    //title
+    labels.append("text")
+    .text("Trump Support")
+    .classed("title",true)
+    .attr("text-anchor","middle")
+    .attr("x",margins.left+(graphDim.width/2))
+    .attr("y",margins.top)
+    
+    //x
+    labels.append("text")
+        .text("PCT of White")
+         .classed("label",true)
+        .attr("text-anchor","middle")
+        .attr("x",margins.left+(graphDim.width/2))
+    .attr("y",margins.top+graphDim.height+margins.bottom)
+
+    //y
+    labels.append("g")
+    .attr("transform","translate(20,"+ 
+              (margins.top+(graphDim.height/2))+")")
+        .append("text")
+        .text("PCT Voting for Trump")
+        .classed("label",true)
+   .attr("text-anchor","middle")
+    .attr("transform","rotate(270)")
+   
 }
 
 
 var drawLegend = function(graphDim,margins)
 {
+    var legend = d3.select("svg")
+       .append("g")
+    .classed("legend",true)
+    .attr("transform","translate("+(margins.left+graphDim.width)+","+(margins.top+10)+")");
     
- 
+    
+    
    var categories = [
        {
            class:"lessCollege",
@@ -97,10 +140,34 @@ var drawLegend = function(graphDim,margins)
            name:"High unemployment"
        }
     ]
-
-
+   
+   var entries = legend.selectAll("g")
+   .data(categories)
+   .enter()
+   .append("g")
+   .classed("legendEntry",true)
+   .attr("class",function(category)
+        {
+       return category.class;
+   })
     
+     
+   .attr("transform",function(category,index)
+        {
+     return  "translate(0,"+(index*20)+")"
+   })
+   
+   entries.append("rect")
+   .attr("width",10)
+    .attr("height",10)
     
+    entries.append("text")
+         .text(function(catergory)
+              {
+        return catergory.name
+    })
+    .attr("x",15)
+    .attr("y",10)
     
 }
 
@@ -110,7 +177,7 @@ var initGraph = function(counties)
     //size of screen
     var screen = {width:800,height:600}
     //how much space on each side
-    var margins = {left:30,right:20,top:20,bottom:30}
+    var margins = {left:50,right:200,top:40,bottom:40}
     
     
     
